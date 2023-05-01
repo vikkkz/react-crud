dbcea5f8284d956ba9b0032acd3a8b8d11c0fa39
vikas g n <vikas@scienstechnologies.in>
2023-05-01 17:18:01 +0530

    create.js as changed
    new comits

---
+++ git_push_pull.sh
@@ -0,0 +1,13 @@
+#creating file in github and git pull done with it and redirected to hello.txt
+git clone 
+cd repo/
+ls 
+git fetch
+git status 
+cat sample > hello
+git status hello
+git add hello
+git status 
+git commit -m "hello added"
+git status 
+git push
